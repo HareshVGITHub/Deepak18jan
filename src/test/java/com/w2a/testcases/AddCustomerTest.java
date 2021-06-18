190bc38a4e4888ee0b68e220bff13e176f7bd874
Haresh Vaity <vaityharesh@gmail.com>
2021-06-18 21:17:15 +0530

    Deepak 2nd Commmmmmmmmmmiiiiitttttttt

--- src/test/java/com/w2a/testcases/AddCustomerTest.java
+++ src/test/java/com/w2a/testcases/AddCustomerTest.java
@@ -19,6 +19,7 @@ public class AddCustomerTest extends TestBase {
 		
 		
 		// Deepak made changes - 1st Commit
+		// This is Deepak 2nd Commit 18 June
 		if(!data.get("runmode").equals("Y")){
 			
 			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
